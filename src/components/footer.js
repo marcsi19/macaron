6fe964f56f6bf971085202b869ef6760d79d1e02
Mariann <mariann19@gmail.com>
2019-08-29 16:58:56 -0700

    footer scss

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,30 +6,32 @@ import instagram from '../img/instagram_icon.svg'
 class Footer extends Component {
   render() {
     return (
-      <div>
-        <div>
-          <div>We are committed to serving the freshest, most delightful treats.</div>
-          <div>
+      <div className="footer_main">
+        <div className="footer_left">
+          <h3>We are committed to serving the freshest, most delightful treats.</h3>
+          <div className="footer_media">
             <img src={facebook} alt="facebook" />
             <img src={twitter} alt="twitter" />
             <img src={instagram} alt="instagram" />
           </div>
-          <div>© 2019 MACARONS</div>
+          <p>© 2019 MACARONS</p>
         </div>
-        <div>
-          <div>Company</div>
-          <ul>
-            <li>About</li>
-            <li>Locations</li>
-            <li>Contact us</li>
-          </ul>
-        </div>
-        <div>
-          <div>Orders</div>
-          <ul>
-            <li>Order Tracker</li>
-            <li>Delivery FAQ</li>
-          </ul>
+        <div className="footer_right">
+          <div>
+            <h4>Company</h4>
+            <ul>
+              <li>About</li>
+              <li>Locations</li>
+              <li>Contact us</li>
+            </ul>
+          </div>
+          <div>
+            <h4>Orders</h4>
+            <ul>
+              <li>Order Tracker</li>
+              <li>Delivery FAQ</li>
+            </ul>
+          </div>
         </div>
       </div>
     )
--- src/components/product.js
+++ src/components/product.js
@@ -8,12 +8,12 @@ const Product = (props) => {
   return (
     <div className="menu_product_main" >
       <div className="menu_product_img">
-        <img src={product.image} />
+        <img src={product.image} alt={product.title} />
       </div>
       <div className="menu_product_description">
         <h3>{product.title}</h3>
         <h4>{product.quantity}</h4>
-        <p>{product.description.split('\n').map((item, i) => <p key={i}>{item}</p>)}</p>
+        <div>{product.description.split('\n').map((item, i) => <p key={i}>{item}</p>)}</div>
       </div>
       <div className="menu_product_shop">
         <p>${product.price} /ea.</p>
