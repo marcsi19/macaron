8434cedcd2aed3a3c7da314b8fdd99bd89a45153
Mariann <mariann19@gmail.com>
2019-08-29 17:52:32 -0700

    cart scss

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## Maca macaron
-
+Open [https://marcsi19.github.io/macaron/] (Website) to view it in the browser.
 In the project directory, you can run:
 ### `npm install`
 ### `npm start`
--- src/components/cart.js
+++ src/components/cart.js
@@ -8,20 +8,28 @@ const Cart = (props) => {
   return (
     <div className={showCart}>
       <button onClick={showingCart} className="cart_close_button">Close</button>
-      <div>My Cart</div>
+      <h3>My Cart</h3>
       < div > {cart.map(item => {
         return (
           <div key={item.id}>
-            <img src={item.image} alt="item" />
-            <div>{item.title}</div>
-            <div>{item.quantity}</div>
-            <div>${item.price}</div>
-            <button onClick={() => props.removeFromCart(item.id)}>Remove</button>
+            <div className="cart_product_main" >
+              <div className="cart_product_img">
+                <img src={item.image} alt="item" />
+              </div>
+              <div className="cart_product_description">
+                <h3>{item.title}</h3>
+                <h4>{item.quantity}</h4>
+                <button onClick={() => props.removeFromCart(item.id)}>Remove</button>
+              </div>
+              <div className="cart_product_price">${item.price}</div>
+            </div>
+
           </div>
         )
       })}</div >
-      <div>Total: ${cart.reduce(((acc, currVal) =>
+      <div className="cart_total">Total   ${cart.reduce(((acc, currVal) =>
         acc + currVal.cartTotal), 0)}</div>
+      <button className="checkout_btn">Checkout</button>
     </div >
 
   )
