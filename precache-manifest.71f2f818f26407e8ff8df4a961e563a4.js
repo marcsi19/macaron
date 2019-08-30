5cab0473dc1d16b747ba61b8a067556615ad4045
Mariann <mariann19@gmail.com>
2019-08-29 17:09:53 -0700

    Updates

---
+++ precache-manifest.71f2f818f26407e8ff8df4a961e563a4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7501cda85caef08071526d46b314cfe2",
+    "url": "/macaron/index.html"
+  },
+  {
+    "revision": "eab663905abc9013eb5c",
+    "url": "/macaron/static/css/main.fd574111.chunk.css"
+  },
+  {
+    "revision": "b05becc174bd2077f138",
+    "url": "/macaron/static/js/2.67b51b05.chunk.js"
+  },
+  {
+    "revision": "eab663905abc9013eb5c",
+    "url": "/macaron/static/js/main.003cecd2.chunk.js"
+  },
+  {
+    "revision": "ae3d328c446779e4cef5",
+    "url": "/macaron/static/js/runtime~main.e5bf82f6.js"
+  },
+  {
+    "revision": "0a05a26d2bba0149ea70eeff34e304c0",
+    "url": "/macaron/static/media/cart_icon.0a05a26d.svg"
+  },
+  {
+    "revision": "21449971fa186026e48603c56730c745",
+    "url": "/macaron/static/media/custom_gift_box.21449971.png"
+  },
+  {
+    "revision": "7f2875221f5e498f25eb1a80c77a0a47",
+    "url": "/macaron/static/media/facebook_icon.7f287522.svg"
+  },
+  {
+    "revision": "58187fb3b5ed75a49da18dcbfc5de120",
+    "url": "/macaron/static/media/instagram_icon.58187fb3.svg"
+  },
+  {
+    "revision": "7b07a2033c3c55996d0a52a56a5171d6",
+    "url": "/macaron/static/media/logo.7b07a203.svg"
+  },
+  {
+    "revision": "88a88bb7c07b3931f12cffbac5980749",
+    "url": "/macaron/static/media/rose.88a88bb7.png"
+  },
+  {
+    "revision": "9eaddc5676d6e7caa709cb18d046e2ea",
+    "url": "/macaron/static/media/twitter_icon.9eaddc56.svg"
+  },
+  {
+    "revision": "5d0b985f243dc9c48af51b9315190a50",
+    "url": "/macaron/static/media/vanilla.5d0b985f.png"
+  }
+]);
